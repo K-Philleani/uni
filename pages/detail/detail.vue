e5a67fcfa9a717641f389e6002e2aff69d496f73
K-Philleani <964202623@qq.com>
2020-11-15 19:28:40 +0800

    基本组件

---
+++ pages/detail/detail.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<text>唱歌</text>
+		<text>跳舞</text>
+		<text>打篮球</text>
+		<view>
+			<text selectable>hahahahahah</text>
+			<text space="nbsp">uuuu    hhh   kkk</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
