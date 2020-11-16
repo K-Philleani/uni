33c1ab8611e09e73d56c6dc21d469fed3faf7a5c
K-Philleani <964202623@qq.com>
2020-11-16 09:31:23 +0800

    下拉刷新

---
+++ pages/list/list.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view v-for="(item, index) in list">{{item}}</view>
+		<button @click="pullFresh">下拉刷新</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		onPullDownRefresh() {
+			this.list = ["后端", "JAVA"]
+			console.log("下拉刷新")
+			uni.stopPullDownRefresh()
+		},
+		data() {
+			return {
+				list: ["前端", "Golang", "Python", "JavaScript"]
+			}
+		},
+		methods: {
+			pullFresh() {
+				uni.startPullDownRefresh()
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
